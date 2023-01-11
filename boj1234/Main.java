7c538297b45c621edafdb7f34f1da2e9c62c816a
You-jisang <dbwltkd1019@naver.com>
2023-01-11 22:30:47 +0900

    feat: test

--- boj1234/Main.java
+++
@@ -1,7 +0,0 @@
-package boj1234;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("hi");
-    }
-}
---
+++ boj_problem/Main.java
@@ -0,0 +1,25 @@
+package boj_problem;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            int num1 = scanner.nextInt();
+            int num2 = scanner.nextInt();
+
+            System.out.println(num1);
+            System.out.println(num2);
+
+            if(num1 == 0 && num2 == 0) break;
+
+            if(num1 > num2) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
+        }
+    }
+}
