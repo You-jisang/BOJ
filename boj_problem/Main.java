6b16355700bbdf3971ce79f169dd9148c3e8d45b
You-jisang <dbwltkd1019@naver.com>
2023-01-11 22:36:19 +0900

    feat: test2

--- boj_problem/Main.java
+++ boj_problem/Main.java
@@ -6,20 +6,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        while (true) {
-            int num1 = scanner.nextInt();
-            int num2 = scanner.nextInt();
+        int num = scanner.nextInt();
+        int sum = 0;
 
-            System.out.println(num1);
-            System.out.println(num2);
-
-            if(num1 == 0 && num2 == 0) break;
-
-            if(num1 > num2) {
-                System.out.println("Yes");
-            } else {
-                System.out.println("No");
-            }
+        for(int i = 1; i <= num; i++) {
+            sum += i;
         }
+
+        System.out.println(sum);
     }
 }
