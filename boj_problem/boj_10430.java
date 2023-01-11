7a2aabb51406331eb4447166ab5a9d8aa8f562d7
You-jisang <dbwltkd1019@naver.com>
2023-01-11 22:51:08 +0900

    feat: java study Day3 BOJ assignment

---
+++ boj_problem/boj_10430.java
@@ -0,0 +1,24 @@
+package boj_problem;
+
+import java.util.Scanner;
+
+public class boj_10430 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int C = scanner.nextInt();
+
+        int[] exam = new int[4];
+
+        exam[0] = (A+B)%C;
+        exam[1] = ((A%C) + (B%C))%C;
+        exam[2] = (A*B)%C;
+        exam[3] = ((A%C) * (B%C))%C;
+
+        for(int x : exam) {
+            System.out.println(x);
+        }
+    }
+}
