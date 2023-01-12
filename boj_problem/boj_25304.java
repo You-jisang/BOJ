6f083bf4dd269f50d723d869675b03db6d8ec807
You-jisang <dbwltkd1019@naver.com>
2023-01-12 21:36:49 +0900

    feat: java study Day4 BOJ assignment

---
+++ boj_problem/boj_25304.java
@@ -0,0 +1,23 @@
+package boj_problem;
+
+import java.util.Scanner;
+
+public class boj_25304 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int totalPrice = scanner.nextInt();
+        int totalBuy = scanner.nextInt();
+
+        int sum = 0;
+
+        for (int i = 0; i < totalBuy; i++) {
+            int price = scanner.nextInt();
+            int buy = scanner.nextInt();
+            sum += price * buy;
+        }
+
+
+        if(totalPrice == sum) System.out.println("Yes");
+        else System.out.println("No");
+    }
+}
---
+++ boj_problem/boj_3003.java
@@ -0,0 +1,27 @@
+package boj_problem;
+
+import java.util.*;
+public class boj_3003 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] chess = {1, 1, 2, 2, 2, 8};
+        int[] find = new int[6];
+        int[] how = new int[6];
+
+        for (int i = 0; i < find.length; i++) {
+            find[i] = scanner.nextInt();
+            how[i] = chess[i] - find[i];
+        }
+        for (int i : how) {
+            System.out.print(i + " ");
+        }
+    }
+}
+/*
+int king;
+        int queen;
+        int rooks 2;
+        int bishops 2;
+        int knights 2;
+        int pawns 8;
+ */
